e9102fba79c5fb8f01c54bc7dee92660008f4792
Shourya Sengupta <sengupta.shourya@gmail.com>
2020-01-13 23:15:59 +0530

    added swagger openapi

--- crud/settings.py
+++ crud/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework_swagger',
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_yasg',
     'employee',
     'product',
 ]
@@ -59,7 +61,7 @@ ROOT_URLCONF = 'crud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
